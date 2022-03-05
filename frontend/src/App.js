48a3f97b4439ec39a92f00e0fec991cd24bd3857
Sammul To <duy5@myumanitoba.ca>
2022-03-04 20:06:48 -0600

    update route the support new pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,44 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 import Home from "./home/pages/Home";
 import Login from "./login/pages/Login";
 import Signup from "./signup/pages/Signup";
-import Account from "./account/pages/Account";
+import Account from "./user/pages/Account";
+import InfoUpdated from './user/pages/InfoUpdated'
+import UpdateAccInfo from './user/pages/UpdateAccInfo';
+import DeleteAccountWarning from './user/pages/DeleteAccountWarning'
+import AccountDeleted from './user/pages/AccountDelected'
 import { AuthContext } from "./shared/context/auth-context";
 import { useAuth } from "./shared/hooks/auth-hook";
 
 function App() {
-  const { token, login, logout, userId } = useAuth();
+  const { token, login, logout, userId, userInfo } = useAuth();
   let routes;
 
   if (token) {
+    //routes for logged in user
     routes = (
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              image="https://media-cldnry.s-nbcnews.com/image/upload/newscms/2020_28/1587661/dogs-age-years-kb-inline-200707.jpg"
-              name="homePageDogsImage"
-            />
-          }
-        />
-        <Route path="/account" element={<Account />} />
-        {/* <Route path="/*" element={<Account />} />  Uncomment to redirect to Account Page if route is not exits*/}
+        <Route path="/" element={<Home/>}/>
+        <Route path="/account" element={<Account/>}/>
+        <Route path="/updateAccInfo" element={<UpdateAccInfo/>}/>
+        <Route path="/deleteaccountwarning" element={<DeleteAccountWarning/>}/>
+        <Route path="/*" element={<Account/>}/>
       </Routes>
     );
   } else {
+    //routes for public
     routes = (
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              image="https://media-cldnry.s-nbcnews.com/image/upload/newscms/2020_28/1587661/dogs-age-years-kb-inline-200707.jpg"
-              name="homePageDogsImage"
-            />
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        {/* <Route path="/*" element={<Home />} Uncomment to redirect to Home Page if route is not exits/> */}
+        <Route path="/"element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/> 
+        <Route path="/profileupdated" element={<InfoUpdated/>}/>
+        <Route path="/accountdeleted" element={<AccountDeleted/>}/>
+        <Route path="/*" element={<Home/>}/>
       </Routes>
     );
   }
@@ -54,6 +48,7 @@ function App() {
         isLoggedIn: !!token,
         token: token,
         userId: userId,
+        userInfo: userInfo,
         login: login,
         logout: logout,
       }}

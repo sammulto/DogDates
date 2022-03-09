92763f2854ec241273034dcc3ac6727ec3fc490b
Zhijie Zheng <92958353+ZhijieZheng-UM@users.noreply.github.com>
2022-03-08 22:32:55 -0600

    Update registerButton.test.js

--- frontend/src/__tests__/home/registerButton.test.js
+++ frontend/src/__tests__/home/registerButton.test.js
@@ -1,11 +1,11 @@
 import {render} from "@testing-library/react";
-import Signup from "../../home/components/RegisterButton";
+import Register from "../../home/components/RegisterButton";
 
 describe('Sign In Component Tests', () => {
 
     test('Render standard component', () => {
-        const {queryByText} = render(<Signup/>);
-        const signIn = queryByText("Sign Up");
-        expect(signIn).toBeTruthy();
+        const {queryByText} = render(<Register/>);
+        const register = queryByText("Sign Up");
+        expect(register).toBeTruthy();
     });
 });

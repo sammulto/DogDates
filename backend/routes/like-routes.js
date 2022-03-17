4421bef3699b09bc0e5d2b0cb5fed9e496f9722d
Sammul To <duy5@myumanitoba.ca>
2022-03-16 23:17:28 -0500

    update routes to match new API

--- backend/routes/like-routes.js
+++ backend/routes/like-routes.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const express = require('express');
+const { check } = require('express-validator');
+
 
 const controller = require('../controllers/like-controller');
 
 const router = express.Router();
 
-//get the list of user's liked users
-router.get('/:uid', controller.getListByUser);
-
-//update a user's list
-router.patch('/:uid', controller.updateListByUser);
+//add the liked user's uid to liked list
+router.patch('/:uid', [
+  check('uid').not().isEmpty()
+  ],controller.addUidToLikeList
+);
 
 module.exports = router;

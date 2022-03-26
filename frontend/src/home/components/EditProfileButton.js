a8fde1c9dd90decb7ad3ad7701cb1ed4b0a62541
Liza Yashin <liza.ya3011@gmail.com>
2022-03-25 20:09:48 -0500

    edit home page when logged in - 3 new buttons

---
+++ frontend/src/home/components/EditProfileButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './EditProfileButton.css'
+
+export default function EditProfileButton() {
+  return (
+    <a className="editProfileBtn" href="/playDates" role="button">
+      Edit My Profile
+    </a>
+  );
+}
---
+++ frontend/src/home/components/MyMatchesButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './MyMatchesButton.css'
+
+export default function MyMatchesButton() {
+  return (
+    <a className="MyMatchesBtn" href="/playDates" role="button">
+      Edit My Profile
+    </a>
+  );
+}

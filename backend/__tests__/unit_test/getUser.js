5dc52b73af380d21a2917249e4160fbf8c944be6
Zhijie Zheng <92958353+ZhijieZheng-UM@users.noreply.github.com>
2022-04-24 05:14:23 -0500

    Update getUser.js

--- backend/__tests__/unit_test/getUser.js
+++ backend/__tests__/unit_test/getUser.js
@@ -17,6 +17,8 @@ let uid1 = "";
 let token1 = "";
 let uid2 = "";
 let token2 = "";
+let uid3 = "";
+let token3 = "";
 
 //setup DB before tests
 beforeAll ( async ()=> {
@@ -52,6 +54,18 @@ beforeAll ( async ()=> {
     token2 = res.body.token;
   });
 
+  await request(app).post('/api/signup').send({
+    email: "abcdef@a.com",
+    password: "123456",
+    ownerName: "Robert",
+    dogName: "Cold",
+    city: "Winnipeg",
+    description: "Hello, I'm Robert and my dog is Cold.",
+    pictures: ["9uf234hf40.jpg"],
+  }).then((res) => {
+    uid3 = res.body.uid;
+    token3 = res.body.token;
+  });
 });
 
 
@@ -64,20 +78,35 @@ afterAll ( async () => {
 
 
 describe('Test get one user info', () => {
-    test('Without authorized token', async () => {
-        return request(app).get('/api/users' + uid1).send({
-        }).expect(401);
+  test('Without authorized token', async () => {
+    return request(app).get('/api/view/' + uid1).send({
+    }).expect(401);
+  })
+
+  test('With wrong uid', async () => {
+    return request(app).get('/api/view/' + uid2).set('Authorization', 'Bearer ' + token1).send({
+    }).expect(401);
+  })
+
+  test('With correct uid, get next user(user3)', async () => {
+    return request(app).get('/api/view/' + uid1).set('Authorization', 'Bearer ' + token1).send({
+    }).expect(200).then((res) => {
+      expect(res.body.uid).toEqual(uid3);
+      expect(res.body.ownerName).toEqual("Robert");
+      expect(res.body.dogName).toEqual("Cold");
+      expect(res.body.city).toEqual("Winnipeg");
+      expect(res.body.description).toEqual("Hello, I'm Robert and my dog is Cold.");
     })
+  })
 
-    test('With wrong uid', async () => {
-        return request(app).get('/api/users/' + "123").set('Authorization', 'Bearer ' + token1).send({
-
-
-        }).expect(404);
+  test('With correct uid, get next user(user2)', async () => {
+    return request(app).get('/api/view/' + uid1).set('Authorization', 'Bearer ' + token1).send({
+    }).expect(200).then((res) => {
+      expect(res.body.uid).toEqual(uid2);
+      expect(res.body.ownerName).toEqual("Christina");
+      expect(res.body.dogName).toEqual("Happy");
+      expect(res.body.city).toEqual("Winnipeg");
+      expect(res.body.description).toEqual("Hello, I'm Christina and my dog is Happy.");
     })
-
-    test('With correct uid', async () => {
-        return request(app).get('/api/users/' + uid1).set('Authorization', 'Bearer ' + token1).send({
-        }).expect(200);
-    })
+  })
 });

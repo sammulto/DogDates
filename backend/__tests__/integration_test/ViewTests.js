d9a12bc27eb21c4275a5e6fce400538a45ac368f
Zhijie Zheng <92958353+ZhijieZheng-UM@users.noreply.github.com>
2022-04-24 05:15:38 -0500

    Update ViewTests.js
    https://github.com/DogDatesComp4350/DogDates/issues/114

--- backend/__tests__/integration_test/ViewTests.js
+++ backend/__tests__/integration_test/ViewTests.js
@@ -66,7 +66,6 @@ beforeAll(async () => {
     uid3 = res.body.uid;
     token3 = res.body.token;
   });
-
 });
 
 
@@ -79,40 +78,7 @@ afterAll(async () => {
 
 
 describe('Test get one user info', () => {
-  test('Without authorized token', async () => {
-    return request(app).get('/api/view/' + uid1).send({
-    }).expect(401);
-  })
-
-  test('With wrong uid', async () => {
-    return request(app).get('/api/view/' + uid2).set('Authorization', 'Bearer ' + token1).send({
-
-
-    }).expect(401);
-  })
-
-  test('With correct uid, get next user(user3)', async () => {
-    return request(app).get('/api/view/' + uid1).set('Authorization', 'Bearer ' + token1).send({
-    }).expect(200).then((res) => {
-      expect(res.body.uid).toEqual(uid3);
-      expect(res.body.ownerName).toEqual("Robert");
-      expect(res.body.dogName).toEqual("Cold");
-      expect(res.body.city).toEqual("Winnipeg");
-      expect(res.body.description).toEqual("Hello, I'm Robert and my dog is Cold.");
-    })
-  })
-
-  test('With correct uid, get next user(user2)', async () => {
-    return request(app).get('/api/view/' + uid1).set('Authorization', 'Bearer ' + token1).send({
-    }).expect(200).then((res) => {
-      expect(res.body.uid).toEqual(uid2);
-      expect(res.body.ownerName).toEqual("Christina");
-      expect(res.body.dogName).toEqual("Happy");
-      expect(res.body.city).toEqual("Winnipeg");
-      expect(res.body.description).toEqual("Hello, I'm Christina and my dog is Happy.");
-    })
-  })
-
+  
   test('With correct uid, get next user(user3)', async () => {
     return request(app).get('/api/view/' + uid1).set('Authorization', 'Bearer ' + token1).send({
     }).expect(200).then((res) => {
